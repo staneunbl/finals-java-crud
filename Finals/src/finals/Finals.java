6d66b6eedb4a02c2728a9534e819db88c7eb0c08
Shayyanne Marasigan <shayyannedominiq.marasigan@letran.edu.ph>
2024-12-08 00:21:25 +0800

    first commit

---
+++ Finals/src/finals/Finals.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finals;
+
+/**
+ *
+ * @author user
+ */
+public class Finals {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginForm loginform = new LoginForm();
+        loginform.run();
+    }
+}
